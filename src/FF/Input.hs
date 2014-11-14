294639a0c54b092169d3de86c1f81643582f71ab
Trevor Elliott <trevor@galois.com>
2014-11-13 23:33:34 -0800

    Less warnings, and more PDDL parsing

--- src/FF/Input.hs
+++ src/FF/Input.hs
@@ -8,18 +8,17 @@
 module FF.Input where
 
 import           Control.Applicative ( Applicative(..), Alternative(..) )
-import           Control.Monad ( unless, when, msum  )
+import           Control.Monad ( when )
 import qualified Data.Attoparsec.Text.Lazy as A
-import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified Data.Text as S
-import qualified Data.Text.Lazy as L
 
 data PDDL = PDDLProblem Problem
           | PDDLDomain Domain
             deriving (Show)
 
 type Name = S.Text
+type Pred = Name
 
 type Var  = S.Text
 
@@ -29,6 +28,7 @@ data Domain = Domain { dName     :: Name
                      , dConsts   :: TypedList Name
                      , dPredSigs :: [PredSig]
                      , dFuns     :: TypedList Function
+                     , dCons     :: [Con]
                      } deriving (Show)
 
 emptyDomain :: Name -> Domain
@@ -38,6 +38,7 @@ emptyDomain n = Domain { dName     = n
                        , dConsts   = UntypedList []
                        , dPredSigs = []
                        , dFuns     = UntypedList []
+                       , dCons     = []
                        }
 
 data Problem = Problem { pName     :: !Name
@@ -72,9 +73,13 @@ data TypedList a = UntypedList [a]
                  | TypedList [Typed a]
                    deriving (Show,Functor)
 
-data Typed a = Typed { tType :: Name
+data Typed a = Typed { tType :: Type
                      , tVal  :: a
                      } deriving (Show,Functor)
+
+data Type = Type Name
+          | Either [Name]
+            deriving (Show)
 
 data PredSig = PredSig { psName :: Name
                        , psArgs :: TypedList Name
@@ -83,6 +88,40 @@ data PredSig = PredSig { psName :: Name
 data Function = Function { funName :: Name
                          , funArgs :: TypedList Name
                          } deriving (Show)
+
+data Term = TVar Var
+          | TName Name
+            deriving (Show)
+
+data GoalDesc = GDAnd [GoalDesc]
+              | GDOr [GoalDesc]
+              | GDNot GoalDesc
+              | GDImply GoalDesc GoalDesc
+              | GDExists (TypedList Var) GoalDesc
+              | GDForall (TypedList Var) GoalDesc
+              | GDLit (Literal Term)
+                deriving (Show)
+
+data Con = CAnd [GoalDesc]
+         | CForall (TypedList Name) Con
+         | CAtEnd GoalDesc
+         | CAlways GoalDesc
+         | CSometime GoalDesc
+         | CWithin Int GoalDesc
+         | CAtMostOnce GoalDesc
+         | CSometimeAfter GoalDesc GoalDesc
+         | CSometimeBefore GoalDesc GoalDesc
+         | CAlwaysWithin Int GoalDesc GoalDesc
+         | CHoldDuring Int Int GoalDesc
+         | CHoldAfter Int GoalDesc
+           deriving (Show)
+
+data Literal a = Literal Bool (Formula a)
+                 deriving (Show)
+
+data Formula a = FPred Pred [a]
+               | FEq a a
+                 deriving (Show)
 
 
 -- Utilities -------------------------------------------------------------------
@@ -114,11 +153,11 @@ expandRequirement req = [req]
 data SExp = SList [SExp]
           | SString !S.Text
           | SSymbol !S.Text
-          | SInt !Integer
+          | SInt !Int
             deriving (Show)
 
 comment :: A.Parser ()
-comment  = A.option () $ do A.char ';'
+comment  = A.option () $ do _ <- A.char ';'
                             loop
   where
   loop = do done <- A.choice [ do c <- A.satisfy A.isEndOfLine
@@ -137,16 +176,14 @@ sexp :: A.Parser SExp
 sexp  = (A.skipSpace *> comment *> (slist <|> sstring <|> ssymbol <|> sint))
   A.<?> "sexp"
   where
-  spaces = A.many1 A.space
-
-  slist = do A.char '('
+  slist = do _    <- A.char '('
              exps <- A.manyTill (sexp <* A.skipSpace) (A.char ')')
              return (SList exps)
        A.<?> "slist"
 
-  sstring = do A.char '"'
+  sstring = do _   <- A.char '"'
                str <- A.takeWhile (/= '\"')
-               A.char '"'
+               _   <- A.char '"'
                return (SString str)
          A.<?> "string"
 
@@ -167,6 +204,15 @@ parseVar _                                              = fail "expected a var"
 parseName :: SExp -> A.Parser Name
 parseName (SSymbol name) = return name
 parseName _              = fail "expected a name"
+
+parseType :: SExp -> A.Parser Type
+parseType (SSymbol name)                 = return (Type name)
+parseType (SList (SSymbol "either":tys)) = Either `fmap` mapM parseName tys
+parseType _                              = fail "expected a type"
+
+parseTerm :: SExp -> A.Parser Term
+parseTerm e = (TName `fmap` parseName e)
+          <|> (TVar  `fmap` parseVar  e)
 
 pddl :: A.Parser [PDDL]
 pddl  = loop
@@ -189,9 +235,11 @@ pddlElem (SList (SSymbol "define" : rest)) =
         PDDLProblem `fmap` problem name rest'
 
     _ -> fail "invalid top-level element"
+pddlElem _ =
+  fail "expected pddl element"
 
 domain :: Name -> [SExp] -> A.Parser Domain
-domain name = loop Set.empty (emptyDomain name)
+domain domName = loop Set.empty (emptyDomain domName)
   where
   loop seen _ (SList (SSymbol name : _) : _)
     | name `Set.member` seen =
@@ -220,7 +268,7 @@ domain name = loop Set.empty (emptyDomain name)
   loop _ dom [] =
        return dom
 
-  loop _ dom _ =
+  loop _ _ _ =
        fail "invalid domain specification"
 
 problem :: Name -> [SExp] -> A.Parser Problem
@@ -228,7 +276,7 @@ problem name def =
      undefined
 
 requireKey :: SExp -> A.Parser Requirement
-requireKey exp = case exp of
+requireKey e = case e of
   SSymbol ":strips"                    -> return ReqStrips
   SSymbol ":typing"                    -> return ReqTyping
   SSymbol ":negative-preconditions"    -> return ReqNegativePreconditions
@@ -263,12 +311,13 @@ typedList parse es = loop [] es A.<?> "typed list"
 
   loop as [] = return (UntypedList (reverse as))
 
-  addType as (SSymbol ty : rest)
-    | null rest =    return (TypedList as')
-    | otherwise = do TypedList tys <- typed [] rest
-                     return (TypedList (as' ++ tys))
-    where
-    as' = [ Typed ty a | a <- reverse as ]
+  addType as (sym:rest) =
+    do ty <- parseType sym
+       let as' = [ Typed ty a | a <- reverse as ]
+       if null rest
+          then return (TypedList as')
+          else do TypedList tys <- typed [] rest
+                  return (TypedList (as' ++ tys))
 
   addType _ _ = fail "expected a type"
 
@@ -278,7 +327,7 @@ typedList parse es = loop [] es A.<?> "typed list"
     _           -> do a <- parse e
                       typed (a:as) rest
 
-  typed as [] = fail "set of variables without a final type"
+  typed _ [] = fail "set of variables without a final type"
 
 predSig :: SExp -> A.Parser PredSig
 predSig (SList (SSymbol name : args)) =
@@ -292,3 +341,101 @@ function :: SExp -> A.Parser Function
 function (SList (SSymbol name : args)) =
   do as <- typedList parseVar args
      return Function { funName = name, funArgs = as }
+function _ =
+     fail "expected function"
+
+goalDesc :: SExp -> A.Parser GoalDesc
+goalDesc (SList (SSymbol sym : rest))
+  | sym == "and" =
+       GDAnd `fmap` mapM goalDesc rest
+
+  | sym == "or" =
+       GDOr  `fmap` mapM goalDesc rest
+
+  | sym == "imply", [p,q] <- rest =
+    do a <- goalDesc p
+       b <- goalDesc q
+       return (GDImply a b)
+
+  | sym == "exists", [SList tys,body] <- rest =
+    do args <- typedList parseVar tys
+       gd   <- goalDesc body
+       return (GDExists args gd)
+
+  | sym == "forall", [SList tys,body] <- rest =
+    do args <- typedList parseVar tys
+       gd   <- goalDesc body
+       return (GDForall args gd)
+
+goalDesc e =
+   GDLit `fmap` literal parseTerm e
+
+
+constraint :: SExp -> A.Parser Con
+constraint (SList (SSymbol sym : cons))
+
+  | sym == "and" =
+       CAnd `fmap` mapM goalDesc cons
+
+  | sym == "forall", [SList vars,body] <- cons =
+    do args <- typedList parseVar vars
+       con  <- constraint body
+       return (CForall args con)
+
+  | sym == "at", [SSymbol "end",body] <- cons =
+       CAtEnd `fmap` goalDesc body
+
+  | sym == "always", [body] <- cons =
+       CAlways `fmap` goalDesc body
+
+  | sym == "sometime", [body] <- cons =
+       CSometime `fmap` goalDesc body
+
+  | sym == "within", [SInt n,body] <- cons =
+       CWithin n `fmap` goalDesc body
+
+  | sym == "at-most-once", [body] <- cons =
+       CAtMostOnce `fmap` goalDesc body
+
+  | sym == "sometime-after", [a,b] <- cons =
+    do x <- goalDesc a
+       y <- goalDesc b
+       return (CSometimeAfter x y)
+
+  | sym == "sometime-before", [a,b] <- cons =
+    do x <- goalDesc a
+       y <- goalDesc b
+       return (CSometimeBefore x y)
+
+  | sym == "always-within", [SInt n, a, b] <- cons =
+    do x <- goalDesc a
+       y <- goalDesc b
+       return (CAlwaysWithin n x y)
+
+  | sym == "hold-during", [SInt a, SInt b, body] <- cons =
+       CHoldDuring a b `fmap` goalDesc body
+
+  | sym == "hold-after", [SInt n, body] <- cons =
+       CHoldAfter n `fmap` goalDesc body
+
+constraint _ = fail "expected <con-GD>"
+
+
+literal :: (SExp -> A.Parser a) -> SExp -> A.Parser (Literal a)
+literal parse e@(SList (SSymbol sym : args))
+  | sym == "not" = Literal False `fmap` formula parse (SList args)
+  | otherwise    = Literal True  `fmap` formula parse e
+literal _ _ = fail "expected <literal>"
+
+
+formula :: (SExp -> A.Parser a) -> SExp -> A.Parser (Formula a)
+formula parse (SList (SSymbol sym : args))
+  | sym == "=", [a,b] <- args =
+    do x <- parse a
+       y <- parse b
+       return (FEq x y)
+
+  | otherwise =
+       FPred sym `fmap` mapM parse args
+
+formula _ _ = fail "expected <atomic formula>"
