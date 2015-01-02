85db99c9bc171f7f550e2ecc68e926225ce0513c
Trevor Elliott <trevor@galois.com>
2015-01-02 13:30:48 -0800

    Implement greedy-best-first as a fall back strategy

--- src/FF/Planner.hs
+++ src/FF/Planner.hs
@@ -2,13 +2,14 @@
 module FF.Planner where
 
 import           FF.ConnGraph
-import           FF.Extract ( extractPlan, helpfulActions )
+import           FF.Extract ( extractPlan, helpfulActions, layer1 )
 import           FF.Fixpoint
 import qualified FF.Input as I
 import qualified FF.RefSet as RS
 
 import           Control.Monad ( guard )
 import           Data.Array.IO ( readArray )
+import qualified Data.Foldable as F
 import           Data.IORef ( IORef, newIORef, readIORef, writeIORef )
 import           Data.List ( minimumBy )
 import           Data.Maybe ( isJust, fromMaybe, catMaybes )
@@ -26,7 +27,7 @@ findPlan dom plan =
      mb <- enforcedHillClimbing hash cg s0 goal
      mkPlan cg =<< if isJust mb
                       then return mb
-                      else greedyBestFirst cg s0 goal
+                      else greedyBestFirst hash cg s0 goal
   where
   mkPlan cg (Just effs) = Just `fmap` mapM (getOper cg) effs
   mkPlan _  Nothing     = return Nothing
@@ -35,6 +36,9 @@ findPlan dom plan =
     do Effect { .. } <- readArray (cgEffects cg) eref
        Oper { .. }   <- readArray (cgOpers cg) eOp
        return oName
+
+
+-- Enforced Hill Climbing ------------------------------------------------------
 
 type Steps = [EffectRef]
 
@@ -59,52 +63,92 @@ findBetterState hash cg h s goal =
 
        Just (plan,goalSet) ->
          do acts <- helpfulActions cg plan goalSet
-            case acts of
-              []  -> return Nothing
-
-              [ref] -> do mbH <- lookupHeuristic ref
-                          case mbH of
-                            Just (h',_,_) | h' < h -> return mbH
-                            _                      -> return Nothing
-
-              -- find the best option
-              as -> do mbs <- mapM lookupHeuristic as
-                       let fst3 (a,_,_) = a
-                           res@(h',_,_) = minimumBy (comparing fst3)
-                                              (catMaybes mbs)
-                       return $ do guard (h' < h)
-                                   return res
+            mb   <- successor hash cg s goal acts
+            return $! do res@(h',_,_) <- mb
+                         guard (h' > h)
+                         return res
 
        -- no plan
        Nothing ->
          return Nothing
 
+-- | Apply effects to the current state, returning the minimal next choice (if
+-- it exists).
+successor :: Hash -> ConnGraph -> State -> Goals
+          -> [EffectRef]
+          -> IO (Maybe (Int,State,EffectRef))
+successor hash cg s goal refs =
+  do mbs <- mapM heuristic refs
+     case catMaybes mbs of
+       [] -> return Nothing
+       hs -> return $! Just $! minimumBy (comparing fst3) hs
+
   where
 
-  -- compute the heuristic value for the state that results after applying the
-  -- given effect, and hash it.
-  lookupHeuristic ref =
-    do Effect { .. } <- readArray (cgEffects cg) ref
-       let s' = (s `RS.union` eAdds) RS.\\ eDels
-       mb <- lookupState hash s'
-       case mb of
-         Just h' -> return (Just (h', s', ref))
-         Nothing -> do mbH <- computeHeuristic cg s' goal
-                       hashState hash s' (fromMaybe maxBound mbH)
-                       return $ do h' <- mbH
-                                   return (h', s', ref)
+  fst3 (a,_,_) = a
 
--- | Compute the size of the relaxed plan produced by the given starting state
--- and goals.
-computeHeuristic :: ConnGraph -> State -> Goals -> IO (Maybe Int)
-computeHeuristic cg s goal =
-  do _  <- buildFixpoint cg s goal
-     mb <- extractPlan cg goal
-     return $ do (plan,_) <- mb
-                 return (length plan)
+  heuristic ref =
+    do s'  <- applyEffect cg ref s
+       mbH <- computeHeuristic hash cg s' goal
+       return $ do h' <- mbH
+                   return (h',s',ref)
 
-greedyBestFirst :: ConnGraph -> State -> Goals -> IO (Maybe Steps)
-greedyBestFirst cg s0 goals = error "greedy best-first"
+-- | Apply an effect to the state given, returning a new state.
+applyEffect :: ConnGraph -> EffectRef -> State -> IO State
+applyEffect cg ref s =
+  do Effect { .. } <- readArray (cgEffects cg) ref
+     return $! (s `RS.union` eAdds) RS.\\ eDels
+
+-- compute the heuristic value for the state that results after applying the
+-- given effect, and hash it.
+computeHeuristic :: Hash -> ConnGraph -> State -> Goals -> IO (Maybe Int)
+computeHeuristic hash cg s goal =
+  do mb <- lookupState hash s
+     case mb of
+       -- return the cached heuristic
+       Just h' ->    return (Just h')
+
+       -- compute and cache the heuristic
+       Nothing -> do mbH <- heuristic
+                     hashState hash s (fromMaybe maxBound mbH)
+                     return mbH
+  where
+
+  -- Compute the size of the relaxed plan produced by the given starting state
+  -- and goals.
+  heuristic =
+    do _  <- buildFixpoint cg s goal
+       mb <- extractPlan cg goal
+       return $ do (plan,_) <- mb
+                   return (length plan)
+
+
+-- Greedy Best-first Search ----------------------------------------------------
+
+greedyBestFirst :: Hash -> ConnGraph -> State -> Goals -> IO (Maybe Steps)
+greedyBestFirst hash cg s0 goal = loop Seq.empty s0 maxBound
+  where
+
+  -- when the heuristic value hits zero, the goals have been achieved
+  loop effs _ 0 =
+       return (Just (F.toList effs))
+
+  loop effs s h =
+    do _      <- buildFixpoint cg s goal
+       mbPlan <- extractPlan cg goal
+       case mbPlan of
+
+         -- found a plan, compute the heuristic for each action in the first
+         -- layer
+         Just (plan,_) ->
+           do mb <- successor hash cg s goal =<< layer1 cg plan
+              case mb of
+                Just (h',s',ref) -> loop (effs Seq.|> ref) s' (min h h')
+                Nothing          -> return Nothing
+
+         -- can't find a path to the goal from here
+         Nothing ->
+              return Nothing
 
 
 -- State Hashing ---------------------------------------------------------------
