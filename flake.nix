af2ea7f496f3c076059ec322260f74a19112e859
Trevor Elliott <awesomelyawesome@gmail.com>
2023-11-12 11:45:38 -0800

    Add a nix flake to manage the dev environment

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development environment for elliottt/story";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-wasi" ];
+        };
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust
+            pkgs.rust-analyzer
+          ];
+        };
+      }
+    );
+}
